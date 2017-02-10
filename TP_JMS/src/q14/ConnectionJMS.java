9285cb1660a65431dda4a4a9f7248521a3c81f6a
Ricardo <rr.ricci@gmail.com>
2017-02-10 18:06:16 +0100

    Q14

---
+++ TP_JMS/src/q14/ConnectionJMS.java
@@ -0,0 +1,59 @@
+package q14;
+
+import javax.jms.JMSException;
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicSession;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class ConnectionJMS {
+	private Topic topic;
+	private TopicConnection connection;
+	private TopicSession session;
+	
+	public void creerConnexion(String nom) throws NamingException, JMSException {
+		InitialContext ic = new InitialContext();
+		TopicConnectionFactory factory = (TopicConnectionFactory) ic.lookup("jms/tpJMSTopicConnFactory");
+		
+		topic = (Topic) ic.lookup("jms/tpJMSTopicDestination");
+		connection = factory.createTopicConnection();
+		
+		
+		if (nom != null && ! nom.isEmpty())
+			connection.setClientID(nom);
+		
+		
+		session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+	}
+	
+	/*
+	public void creerSession() throws JMSException {
+		session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+	}
+	*/
+	
+	public void activerConnexion() throws JMSException {
+		connection.start();
+	}
+	
+	public void deconnecter() throws JMSException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public TopicConnection getConnection() {
+		return connection;
+	}
+
+	public TopicSession getSession() {
+		return session;
+	}
+}
---
+++ TP_JMS/src/q14/Desinscription.java
@@ -0,0 +1,28 @@
+package q14;
+
+import javax.jms.Session;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicSession;
+import javax.naming.InitialContext;
+
+public class Desinscription {
+	public static void main(String[] args) {
+		String nom = args[0];
+		
+		try {
+			InitialContext ic = new InitialContext();
+			TopicConnectionFactory factory = (TopicConnectionFactory) ic.lookup("jms/tpJMSTopicConnFactory");
+			TopicConnection connection = factory.createTopicConnection();
+			connection.setClientID(nom);
+			
+			TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+			session.unsubscribe(nom);
+			
+			System.out.println("Recepteur desinscrit " + nom);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
